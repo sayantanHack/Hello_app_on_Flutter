00bc0f590d269206bc15cf9ff619400e628ed022
sayantanHack <sayantandatta97@gmail.com>
2018-09-04 03:44:56 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new MyApp());
+
+}
+//  This is an stateless widget -------  uncomment the bellow code & test it
+/*
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Hellow App", // title can also be added directly like title: "Hello App"
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: Text("Home page of Flutter"),
+        ),
+        body: new Center(child: new Text("Hello world !!! This is Sayantan`s App"),)
+      ),
+    );
+  }
+}
+*/
+// Lets make some
