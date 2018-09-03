ae8743431c1170578249061e78511d5c3b040500
sayantanHack <sayantandatta97@gmail.com>
2018-09-04 04:01:08 +0530

    Added another state full widget but the stateful widget has no stateful value.

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,36 @@ void main(){
 
 }
 //  This is an stateless widget -------  uncomment the bellow code & test it
-/*
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: "Hellow App", // title can also be added directly like title: "Hello App"
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: Text("Home page of Flutter"),
-        ),
-        body: new Center(child: new Text("Hello world !!! This is Sayantan`s App"),)
-      ),
+      title: "My App",
+      home: new HomePage(),
     );
   }
 }
-*/
-// Lets make some
+
+
+// Lets make some statefull widget
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+//   If there have underscore before any thing so then it is a private class or private obj , or private method
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text("Home Page"),
+
+      ),
+      body: new Center(child: new Text("Hello Statefulll widget")),
+    );
+  }
+}
+
